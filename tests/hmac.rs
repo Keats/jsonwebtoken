0e09b2a26efc772a46a372dc78580d765e6edc53
Vincent Prouillet <balthek@gmail.com>
2020-11-17 14:28:07 +0100

    Bytes everywhere for sign/verify

--- tests/hmac.rs
+++ tests/hmac.rs
@@ -17,7 +17,7 @@ pub struct Claims {
 #[test]
 fn sign_hs256() {
     let result =
-        sign("hello world", &EncodingKey::from_secret(b"secret"), Algorithm::HS256).unwrap();
+        sign("hello world".as_bytes(), &EncodingKey::from_secret(b"secret"), Algorithm::HS256).unwrap();
     let expected = "c0zGLzKEFWj0VxWuufTXiRMk5tlI5MbGDAYhzaxIYjo";
     assert_eq!(result, expected);
 }
@@ -26,7 +26,7 @@ fn sign_hs256() {
 fn verify_hs256() {
     let sig = "c0zGLzKEFWj0VxWuufTXiRMk5tlI5MbGDAYhzaxIYjo";
     let valid =
-        verify(sig, "hello world", &DecodingKey::from_secret(b"secret"), Algorithm::HS256).unwrap();
+        verify(sig, "hello world".as_bytes(), &DecodingKey::from_secret(b"secret"), Algorithm::HS256).unwrap();
     assert!(valid);
 }
 
--- tests/rsa/mod.rs
+++ tests/rsa/mod.rs
@@ -28,9 +28,9 @@ fn round_trip_sign_verification_pem_pkcs1() {
 
     for &alg in RSA_ALGORITHMS {
         let encrypted =
-            sign("hello world", &EncodingKey::from_rsa_pem(privkey_pem).unwrap(), alg).unwrap();
+            sign("hello world".as_bytes(), &EncodingKey::from_rsa_pem(privkey_pem).unwrap(), alg).unwrap();
         let is_valid =
-            verify(&encrypted, "hello world", &DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), alg)
+            verify(&encrypted, "hello world".as_bytes(), &DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), alg)
                 .unwrap();
         assert!(is_valid);
     }
@@ -43,9 +43,9 @@ fn round_trip_sign_verification_pem_pkcs8() {
 
     for &alg in RSA_ALGORITHMS {
         let encrypted =
-            sign("hello world", &EncodingKey::from_rsa_pem(privkey_pem).unwrap(), alg).unwrap();
+            sign("hello world".as_bytes(), &EncodingKey::from_rsa_pem(privkey_pem).unwrap(), alg).unwrap();
         let is_valid =
-            verify(&encrypted, "hello world", &DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), alg)
+            verify(&encrypted, "hello world".as_bytes(), &DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), alg)
                 .unwrap();
         assert!(is_valid);
     }
@@ -57,9 +57,9 @@ fn round_trip_sign_verification_der() {
     let pubkey_der = include_bytes!("public_rsa_key.der");
 
     for &alg in RSA_ALGORITHMS {
-        let encrypted = sign("hello world", &EncodingKey::from_rsa_der(privkey_der), alg).unwrap();
+        let encrypted = sign("hello world".as_bytes(), &EncodingKey::from_rsa_der(privkey_der), alg).unwrap();
         let is_valid =
-            verify(&encrypted, "hello world", &DecodingKey::from_rsa_der(pubkey_der), alg).unwrap();
+            verify(&encrypted, "hello world".as_bytes(), &DecodingKey::from_rsa_der(pubkey_der), alg).unwrap();
         assert!(is_valid);
     }
 }
