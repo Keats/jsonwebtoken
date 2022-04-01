1d7e9d6c1b64bc94eeeda510e7897c3a28b3daca
Quinnan Gill <qgill@forallsecure.com>
2022-04-01 10:09:08 -0400

    Added new fuzzer

--- fuzz/fuzz_targets/target.rs
+++ fuzz/fuzz_targets/target.rs
@@ -10,17 +10,25 @@ use std::{str, panic};
 
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    // let privkey = include_bytes!("private_ecdsa_key.pk8");
-    // let pubkey = include_bytes!("public_ecdsa_key.pk8");
 
-    // let encrypted =
-    //     sign(data, &EncodingKey::from_ec_der(privkey), Algorithm::ES256).unwrap();
-    // verify(&encrypted, data, &DecodingKey::from_ec_der(pubkey), Algorithm::ES256)
-    //     .unwrap();
     let token = str::from_utf8(data).unwrap();
 
-    panic::catch_unwind(|| {
-        decode_header(token);
-    });
+    decode_header(token);
+
+    // panic::catch_unwind(|| {
+    //     let s = match str::from_utf8(data) {
+    //         Ok(v) => decode_header(v),
+    //         Err(e) => Ok(()),
+    //     };
+    //     // decode_header(token);
+    // });
 });
+
+// fuzzed code goes here
+// let privkey = include_bytes!("private_ecdsa_key.pk8");
+// let pubkey = include_bytes!("public_ecdsa_key.pk8");
+
+// let encrypted =
+//     sign(data, &EncodingKey::from_ec_der(privkey), Algorithm::ES256).unwrap();
+// verify(&encrypted, data, &DecodingKey::from_ec_der(pubkey), Algorithm::ES256)
+//     .unwrap()
