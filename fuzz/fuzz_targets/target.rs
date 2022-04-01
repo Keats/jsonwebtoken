87a067af12d5b0cb4bf8e22b18c3a8cc6851e303
Quinnan Gill <qgill@forallsecure.com>
2022-04-01 10:22:28 -0400

    Added better fuzzer

--- fuzz/fuzz_targets/target.rs
+++ fuzz/fuzz_targets/target.rs
@@ -1,34 +1,10 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-// use jsonwebtoken::{
-//     crypto::{sign, verify},
-//     decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation,
-// };
-use jsonwebtoken::{decode_header};
-use std::{str, panic};
-
-
+use jsonwebtoken;
 
 fuzz_target!(|data: &[u8]| {
 
-    let token = str::from_utf8(data).unwrap();
-
-    decode_header(token);
-
-    // panic::catch_unwind(|| {
-    //     let s = match str::from_utf8(data) {
-    //         Ok(v) => decode_header(v),
-    //         Err(e) => Ok(()),
-    //     };
-    //     // decode_header(token);
-    // });
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = jsonwebtoken::decode_header(s);
+    }
 });
-
-// fuzzed code goes here
-// let privkey = include_bytes!("private_ecdsa_key.pk8");
-// let pubkey = include_bytes!("public_ecdsa_key.pk8");
-
-// let encrypted =
-//     sign(data, &EncodingKey::from_ec_der(privkey), Algorithm::ES256).unwrap();
-// verify(&encrypted, data, &DecodingKey::from_ec_der(pubkey), Algorithm::ES256)
-//     .unwrap()
